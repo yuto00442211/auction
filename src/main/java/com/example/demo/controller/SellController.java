51bb75a83263f2cffb72956f9a6adfca1352d133
3030751 <3030751@30307-51>
2023-08-16 11:30:27 +0900

    コントローラー少し触った

--- src/main/java/com/example/demo/controller/SellController.java
+++ src/main/java/com/example/demo/controller/SellController.java
@@ -2,9 +2,15 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.entity.RegistrationForm;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/sell")
@@ -42,5 +48,17 @@ public class SellController {
         // goodsFormの内容を使ってデータベースに出品情報を登録する処理を呼び出す
        // goodsService.registerGoods(goodsForm);
         return "login"; // 出品完了後、トップページにリダイレクト
+    }
+    
+    @PostMapping("/register")
+    public String register(@RequestBody @Valid RegistrationForm registrationForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            // バリデーションエラーがある場合の処理
+            return "バリデーションエラーが発生しました。";
+        }
+
+        // バリデーションが成功した場合の処理
+        // 登録処理などを行う
+        return "登録が完了しました。";
     }
 }
